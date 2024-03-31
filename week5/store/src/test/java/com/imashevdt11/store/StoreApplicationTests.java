0612b1c2a4e984370d93e8a964443a8473f69843
imashevdt11 <imashevdt@gmail.com>
2024-03-31 12:54:32 +0600

    ...

---
+++ week5/store/src/test/java/com/imashevdt11/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imashevdt11.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
