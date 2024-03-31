cb8011f36182b40278987fcb8658782cd5252820
imashevdt11 <imashevdt@gmail.com>
2024-03-31 12:43:25 +0600

    authentication and registration with jwt

---
+++ week5/store/src/main/java/com/imashevdt11/store/entities/Role.java
@@ -0,0 +1,6 @@
+package com.imashevdt11.store.entities;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ week5/store/src/main/java/com/imashevdt11/store/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.imashevdt11.store.repositories;
+
+import com.imashevdt11.store.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
