86a2c483ee9ac6f7bed2a1ee12ffd1788fbca28d
imashevdt11 <imashevdt@gmail.com>
2024-04-10 12:59:47 +0600

    changes in ProductService

--- week5/store/src/main/java/com/imashevdt11/store/services/ProductService.java
+++ week5/store/src/main/java/com/imashevdt11/store/services/ProductService.java
@@ -2,9 +2,11 @@ package com.imashevdt11.store.services;
 
 import com.imashevdt11.store.dtos.ProductDto;
 import com.imashevdt11.store.entities.Product;
-import com.imashevdt11.store.exceptions.ProductException;
+import com.imashevdt11.store.exceptions.ProductNotFoundException;
 import com.imashevdt11.store.repositories.ProductRepository;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -13,90 +15,96 @@ import java.util.Optional;
 
 @Service
 @AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class ProductService {
 
-    private final ProductRepository repo;
+    ProductRepository productRepository;
+
+    public ProductDto createProduct(ProductDto productDto) {
+        Product product = Product.builder()
+                .name(productDto.getName())
+                .price(productDto.getPrice())
+                .build();
+        Product savedProduct = productRepository.save(product);
+        return convertToProductDto(savedProduct);
+    }
 
     public List<ProductDto> getAllProducts() {
-        List<Product> productList = repo.findAll();
+        List<Product> productList = productRepository.findAll();
         return productList.stream().map(this::convertToProductDto).toList();
     }
 
     public ProductDto getProductById(Long id) {
-        Product product = repo.findById(id)
-                .orElseThrow(() -> new ProductException("Product not found with id: " + id, HttpStatus.NOT_FOUND.value()));
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException("Product not found with id: " + id, HttpStatus.NOT_FOUND.value()));
         return convertToProductDto(product);
     }
 
     public List<ProductDto> getProductsByPrice(Double price) {
-        List<Product> productList = repo.findByPrice(price);
+        List<Product> productList = productRepository.findByPrice(price);
 
         if (productList.isEmpty()) {
-            throw new ProductException("Products with a price equal to " + price + " not found", HttpStatus.NOT_FOUND.value());
+            throw new ProductNotFoundException("Products with a price equal to " + price + " not found", HttpStatus.NOT_FOUND.value());
         }
         return productList.stream().map(this::convertToProductDto).toList();
     }
 
     public List<ProductDto> getProductsBelowPrice(Double price) {
-        List<Product> productList = repo.findProductsByPriceIsLessThan(price);
+        List<Product> productList = productRepository.findProductsByPriceIsLessThan(price);
+
         if (productList.isEmpty()) {
-            throw new ProductException("Products with a price below " + price + " not found", HttpStatus.NOT_FOUND.value());
+            throw new ProductNotFoundException("Products with a price below " + price + " not found", HttpStatus.NOT_FOUND.value());
         }
         return productList.stream().map(this::convertToProductDto).toList();
     }
 
     public List<ProductDto> getProductsAbovePrice(Double price) {
-        List<Product> productList = repo.findProductsByPriceIsGreaterThan(price);
+        List<Product> productList = productRepository.findProductsByPriceIsGreaterThan(price);
+
         if (productList.isEmpty()) {
-            throw new ProductException("Products with a price above " + price + " not found", HttpStatus.NOT_FOUND.value());
+            throw new ProductNotFoundException("Products with a price above " + price + " not found", HttpStatus.NOT_FOUND.value());
         }
         return productList.stream().map(this::convertToProductDto).toList();
     }
 
-    public ProductDto createProduct(ProductDto productDto) {
-        Product product = new Product();
-        product.setName(productDto.getName());
-        product.setPrice(productDto.getPrice());
-        Product savedProduct = repo.save(product);
-        return convertToProductDto(savedProduct);
-    }
-
     public ProductDto updateProduct(Long id, ProductDto productDto) {
-        ProductDto existingProductDto = getProductById(id);
-        if (existingProductDto == null) {
-            throw new ProductException("Product not found with id: " + id, HttpStatus.NOT_FOUND.value());
+        ProductDto product = getProductById(id);
+
+        if (product == null) {
+            throw new ProductNotFoundException("Product not found with id: " + id, HttpStatus.NOT_FOUND.value());
         }
+        product = ProductDto.builder()
+                .name(productDto.getName())
+                .price(productDto.getPrice())
+                .build();
 
-        existingProductDto.setName(productDto.getName());
-        existingProductDto.setPrice(productDto.getPrice());
-
-        Product updatedProduct = repo.save(convertToProductEntity(existingProductDto));
-
+        Product updatedProduct = productRepository.save(convertToProductEntity(product));
         return convertToProductDto(updatedProduct);
     }
 
     public void deleteProductById(Long id) {
-        Optional<Product> optionalProduct = repo.findById(id);
+        Optional<Product> optionalProduct = productRepository.findById(id);
+
         if (optionalProduct.isPresent()) {
-            repo.deleteById(id);
+            productRepository.deleteById(id);
         } else {
-            throw new ProductException("Product not found with id: " + id, HttpStatus.NOT_FOUND.value());
+            throw new ProductNotFoundException("Product not found with id: " + id, HttpStatus.NOT_FOUND.value());
         }
     }
 
     private ProductDto convertToProductDto(Product product) {
-        ProductDto productDto = new ProductDto();
-        productDto.setId(product.getId());
-        productDto.setName(product.getName());
-        productDto.setPrice(product.getPrice());
-        return productDto;
+        return ProductDto.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .price(product.getPrice())
+                .build();
     }
 
     private Product convertToProductEntity(ProductDto productDto) {
-        Product product = new Product();
-        product.setId(productDto.getId());
-        product.setName(productDto.getName());
-        product.setPrice(productDto.getPrice());
-        return product;
+        return Product.builder()
+                .id(productDto.getId())
+                .name(productDto.getName())
+                .price(productDto.getPrice())
+                .build();
     }
 }
